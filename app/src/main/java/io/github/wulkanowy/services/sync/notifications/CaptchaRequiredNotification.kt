8187916e30f5b3b2a846708bb824c35f439c01ae
mibac138 <5672750+mibac138@users.noreply.github.com>
2024-03-14 19:59:00 +0100

    Add captcha required notification

---
+++ app/src/main/java/io/github/wulkanowy/services/sync/notifications/CaptchaRequiredNotification.kt
@@ -0,0 +1,25 @@
+package io.github.wulkanowy.services.sync.notifications
+
+import android.content.Context
+import dagger.hilt.android.qualifiers.ApplicationContext
+import io.github.wulkanowy.R
+import io.github.wulkanowy.data.db.entities.Student
+import io.github.wulkanowy.data.pojos.NotificationData
+import io.github.wulkanowy.ui.modules.Destination
+import javax.inject.Inject
+
+class CaptchaRequiredNotification @Inject constructor(
+    private val appNotificationManager: AppNotificationManager,
+    @ApplicationContext private val context: Context,
+) {
+    suspend fun notify(student: Student) {
+        val notificationData = NotificationData(
+            title = context.getString(R.string.captcha_required_notify_title),
+            content = context.getString(R.string.captcha_required_notify_content),
+            destination = Destination.Dashboard,
+        )
+        appNotificationManager.sendSingleNotification(
+            notificationData, NotificationType.CAPTCHA_REQUIRED, student
+        )
+    }
+}
