44f527651feb3842a7412d74b77802b44f78bee8
mibac138 <5672750+mibac138@users.noreply.github.com>
2024-03-14 21:49:40 +0100

    Configurable captcha required notification

--- app/src/main/java/io/github/wulkanowy/services/sync/notifications/CaptchaRequiredNotification.kt
+++ app/src/main/java/io/github/wulkanowy/services/sync/notifications/CaptchaRequiredNotification.kt
@@ -5,21 +5,42 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import io.github.wulkanowy.R
 import io.github.wulkanowy.data.db.entities.Student
 import io.github.wulkanowy.data.pojos.NotificationData
+import io.github.wulkanowy.data.repositories.PreferencesRepository
 import io.github.wulkanowy.ui.modules.Destination
+import java.time.Instant
 import javax.inject.Inject
+import kotlin.time.toJavaDuration
 
 class CaptchaRequiredNotification @Inject constructor(
     private val appNotificationManager: AppNotificationManager,
+    private val preferencesRepository: PreferencesRepository,
     @ApplicationContext private val context: Context,
 ) {
     suspend fun notify(student: Student) {
+        // Notification disabled
+        if (!preferencesRepository.captchaRequiredNotificationEnabled) return
+
+        val minTimeBetweenNotifications =
+            preferencesRepository.minTimeBetweenCaptchaRequiredNotification
+        val lastNotification =
+            preferencesRepository.lastCaptchaRequiredNotificationTime ?: Instant.MIN
+        if (Instant.now()
+                .isBefore(lastNotification + minTimeBetweenNotifications.toJavaDuration())
+        ) {
+            // Too little time passed between notifications
+            return
+        }
+
         val notificationData = NotificationData(
             title = context.getString(R.string.captcha_required_notify_title),
             content = context.getString(R.string.captcha_required_notify_content),
             destination = Destination.Dashboard,
         )
-        appNotificationManager.trySendSingletonNotification(
+        if (appNotificationManager.trySendSingletonNotification(
             notificationData, NotificationType.CAPTCHA_REQUIRED, student
-        )
+            )
+        ) {
+            preferencesRepository.lastCaptchaRequiredNotificationTime = Instant.now()
+        }
     }
 }
